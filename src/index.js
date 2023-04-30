9c566495b27b91c139281490a68b595296ef6099
Almaz Kubakaev <almazkubakaev2607@gmail.com>
2023-04-30 21:01:35 +0600

    combinereducer, composewithdevtools

--- src/index.js
+++ src/index.js
@@ -2,26 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-
-const initialtState = {
-  cash: 0,
-};
-
-const reducer = (state = initialtState, action) => {
-  switch (action.type) {
-    case "ADD_CASH":
-      return { ...state, cash: state.cash + action.payload };
-    case "GET_CASH":
-      return { ...state, cash: state.cash - action.payload };
-    default:
-      return state;
-  }
-};
-
-const store = configureStore({
-  reducer,
-});
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
