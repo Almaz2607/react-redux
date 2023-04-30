ea14905c54dc29de4a4fe75e3f738f953fada1c3
Almaz Kubakaev <almazkubakaev2607@gmail.com>
2023-04-30 14:00:06 +0600

    starting template

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="buttons">
+        <button>Пополнить счет</button>
+        <button>Снять со счета</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
